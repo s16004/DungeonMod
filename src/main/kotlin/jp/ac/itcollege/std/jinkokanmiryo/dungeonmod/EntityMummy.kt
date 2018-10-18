2ef19bbbb80de5ce4e6028f29125f5b18091bb26
s16004 <s16004@std.it-college.ac.jp>
2018-10-18 14:19:55 +0900

    test

---
+++ src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/EntityMummy.kt
@@ -0,0 +1,6 @@
+package jp.ac.itcollege.std.jinkokanmiryo.dungeonmod
+
+import net.minecraft.entity.EntityLiving
+import net.minecraft.world.World
+
+class EntityMummy(worldIn: World) : EntityLiving(worldIn)
--- src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/SoulEater.kt
+++ src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/SoulEater.kt
@@ -5,55 +5,35 @@ import com.google.common.collect.Multimap
 import net.minecraft.entity.SharedMonsterAttributes
 import net.minecraft.entity.ai.attributes.AttributeModifier
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.entity.player.EntityPlayerMP
-import net.minecraft.init.Blocks
 import net.minecraft.inventory.EntityEquipmentSlot
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.util.*
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
-import net.minecraft.util.EnumActionResult
-import net.minecraft.util.ActionResult
-import net.minecraft.util.EnumHand
-import net.minecraft.world.WorldServer
-
 
 object SoulEater : Item()
 {
     init
     {
-
         this.maxStackSize = 1
         this.unlocalizedName = "souleater"
         this.registryName = ResourceLocation(DungeonMod.ID, "SoulEater")
     }
 
-    private const val CostExp = 1
-    override fun onItemRightClick(world: World, player: EntityPlayer, hand: EnumHand): ActionResult<ItemStack> {
-        // プレイヤーが持っているItemStackは、プレイヤーから取得
-        val stack = player.getHeldItem(hand)
+    override fun onItemUse(player: EntityPlayer?, worldIn: World?, pos: BlockPos?, hand: EnumHand?, facing: EnumFacing?, hitX: Float, hitY: Float, hitZ: Float): EnumActionResult {
 
+        player!!.heal(20.0F)
 
-        if(player.capabilities.isCreativeMode || player.experienceLevel >= CostExp)
-        {
-            if(!player.capabilities.isCreativeMode) player.addExperienceLevel(-CostExp);
-            player.heal(5.0f)
-        }
-
-        player.addExperienceLevel(-CostExp)
-
-        // 結果を返す
-        return ActionResult(EnumActionResult.SUCCESS, stack)
+        return EnumActionResult.SUCCESS
     }
-
 
     override fun getAttributeModifiers(slot: EntityEquipmentSlot, stack: ItemStack): Multimap<String, AttributeModifier> {
         val multimap = HashMultimap.create<String, AttributeModifier>()
 
         if (slot == EntityEquipmentSlot.MAINHAND) {
             multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.name,
-                    AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier",8.0,0))
+                    AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier",7.0,0))
 
             multimap.put(SharedMonsterAttributes.ATTACK_SPEED.name,
                     AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.6, 0))
