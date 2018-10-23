b88f5cbb609a5bdb3fd585a1403de7140f86c492
s16005 <s16005@std.it-college.ac.jp>
2018-10-23 15:04:16 +0900

    commit

---
+++ src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/Soul_of_Grim.kt
@@ -0,0 +1,15 @@
+package jp.ac.itcollege.std.jinkokanmiryo.dungeonmod
+
+import net.minecraft.item.Item
+import net.minecraft.util.ResourceLocation
+
+
+object Soul_of_Grim : Item()
+{
+    init
+    {
+        this.maxStackSize = 64
+        this.unlocalizedName = "soul_of_grim"
+        this.registryName = ResourceLocation(DungeonMod.ID, "Soul_of_Grim")
+    }
+}
--- src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/main.kt
+++ src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/main.kt
@@ -3,16 +3,22 @@ package jp.ac.itcollege.std.jinkokanmiryo.dungeonmod
 import net.minecraft.client.renderer.block.model.ModelResourceLocation
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.entity.Entity
+import net.minecraft.entity.EntityLiving
 import net.minecraft.init.Items
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.util.ResourceLocation
 import net.minecraftforge.client.event.ModelRegistryEvent
 import net.minecraftforge.client.model.ModelLoader
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.fml.client.registry.RenderingRegistry
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLConstructionEvent
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.registry.EntityEntry
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder
 import net.minecraftforge.fml.common.registry.EntityRegistry
 
 @Mod(modid = DungeonMod.ID, name = DungeonMod.Name, version = DungeonMod.Version, modLanguage = "kotlin", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
@@ -29,17 +35,30 @@ class DungeonMod
         {
             override fun getTabIconItem() = ItemStack(Items.STICK)
         }
-
+/*
+        fun createEntry() {
+            EntityEntryBuilder<>
+        }
+*/
         @SubscribeEvent @JvmStatic
         fun registeritem(e: RegistryEvent.Register<Item>) {
             e.registry.register(FrostyRod.setCreativeTab(ctab))
             e.registry.register(SoulEater.setCreativeTab(ctab))
+            e.registry.register(Soul_of_Grim.setCreativeTab(ctab))
         }
+
+        @SubscribeEvent @JvmStatic
+        fun registerentity(e: RegistryEvent.Register<EntityEntry>) {
+
+        }
+
+
 
         @Mod.EventHandler
     fun construction(event: FMLConstructionEvent) {
             MinecraftForge.EVENT_BUS.register(this)
         }
+
         @SubscribeEvent @JvmStatic
     fun registerModels(e: ModelRegistryEvent) {
             ModelLoader.setCustomModelResourceLocation(FrostyRod, 0, ModelResourceLocation(FrostyRod.registryName!!,"inventory"))
