094e597f274a002fadb32954842e8870e3eb9f62
s16005 <s16005@std.it-college.ac.jp>
2018-10-31 15:27:51 +0900

    EntityMummyArcher途中

--- src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/mob/Mobs.kt
+++ src/main/kotlin/jp/ac/itcollege/std/jinkokanmiryo/dungeonmod/mob/Mobs.kt
@@ -3,11 +3,15 @@ package jp.ac.itcollege.std.jinkokanmiryo.dungeonmod.mob
 import jp.ac.itcollege.std.jinkokanmiryo.dungeonmod.DungeonMod
 import jp.ac.itcollege.std.jinkokanmiryo.dungeonmod.client.entity.RenderMummy
 import net.minecraft.entity.EntityLiving
+import net.minecraft.entity.EnumCreatureType
+import net.minecraft.init.Biomes
 import net.minecraft.util.ResourceLocation
 import net.minecraft.world.World
+import net.minecraft.world.biome.BiomeDesert
 import net.minecraftforge.fml.client.registry.RenderingRegistry
 import net.minecraftforge.fml.common.registry.EntityEntry
 import net.minecraftforge.fml.common.registry.EntityEntryBuilder
+import net.minecraftforge.fml.common.registry.ForgeRegistries.BIOMES
 import net.minecraftforge.registries.IForgeRegistry
 
 object Mobs {
@@ -24,6 +28,7 @@ object Mobs {
                 .name(name)
                 .factory(factory)
                 .egg(eggBackground, eggSpot)
+                .spawn(EnumCreatureType.MONSTER, 2000, 1, 50, Biomes.DESERT,Biomes.DESERT_HILLS)
                 .tracker(80, 3, true)
                 .build()
     }
